37b99fae8f5b7ceb2afba4151784be75d751f7c3
mariarizzo <mrizzo@bgsu.edu>
2017-11-13 07:39:40 -0500

    Modify example to check decomposition

--- R/components.R
+++ R/components.R
@@ -3,12 +3,12 @@ distance_components <- function(group, distance) {
   ## total = (between + within) dispersion
   ## distance is a distance object
   ## group is the vector of group labels (integer)
-
+  
   if (class(distance) != "dist") {
     warning("distance argument must be a dist object")
     return(invisible(NA))
   }
-  betweens <- as.dist(between_cluster(group, distance))
+  betweens <- between_cluster(group, distance)
   dst <- as.matrix(distance)
   trt <- factor(group)
   k <- nlevels(trt)
@@ -18,17 +18,17 @@ distance_components <- function(group, distance) {
   y <- as.vector(dst[, 1])
   M <- model.matrix(y ~ 0 + trt)
   G <- t(M) %*% dst %*% M
-  withins <- diag(G)/(2 * n)
+  withins <- diag(G) / (2 * n)
   W <- sum(withins)
   names(withins) <- paste("W", 1:k, sep="")
   B <- total - W
-  list(B = B, W = W, k = k, N = N, sizes = n,
+  list(B = B, W = W, T = total, k = k, N = N, sizes = n, 
        betweens = betweens, withins = withins)
 }
 
 between_cluster <- function(clusters, distance) {
-  #  computes the energy dissimilarity matrix between k samples or clusters
-  #  should be equal to edist with method="disco"
+  #  energy dissimilarity matrix between k samples or clusters
+  #  should be equal to edist with method="discoB"
   #  distance    dist object for the pooled sample
   #  clusters    clustering vector of group labels
   if (class(distance) != "dist") {
@@ -44,17 +44,17 @@ between_cluster <- function(clusters, distance) {
   m <- 1 + c(0, n[1:(k-1)])
   e <- matrix(0, nrow=k, ncol=k)
   dst <- as.matrix(distance)
-
+  
   ## ix: permutation sorts the observations in order of cluster id
   g <- as.integer(clusters)
   ix <- rank(g, ties.method = "first")
-
+  
   ## in balanced design K=N/n and each two-sample statistic is
   ## multiplied by (1/K) * (n_j n_k)/(n_j + n_k)
-  ## for the multisample statistic, unbalanced design:
+  ## for the multisample statistic, unbalanced design: 
   ## replace n by the average (n_j+n_k)/2 to get weight
   ## w_{j,k} = (n_j n_k) / (2 N)  for disco decomposition
-
+  
   for (i in 1:(k - 1)) {
     for (j in (i + 1):k) {
       n1 <- sizes[i]
