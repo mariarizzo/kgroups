b29e7b19c175d4f6fd3f7d2d91f946aaad6d9961
mariarizzo <mrizzo@bgsu.edu>
2018-07-10 17:31:10 -0400

    implementation in Rcpp

--- R/kgroups.R
+++ R/kgroups.R
@@ -1,115 +1,51 @@
-kgroups <- function(x, k, iter.max = 10, nstart = 1, clus = NULL) {
-  ## user level main function to perform k-groups clustering
-  if (!(class(x) == "dist"))
-    x <- dist(x)
-  dst <- as.matrix(x)
-  n <- nrow(x)
 
-  value <- .kgroupsStart(dst, k, iter.max, clus)
+kgroups <- function(x, k, iter.max = 10, nstart = 1, cluster = NULL) {
+  distance <- (class(x) == "dist") 
+  x <- as.matrix(x)
+  if (!is.numeric(x))
+    stop("x must be numeric")
+  n <- nrow(x)
+  if (is.null(cluster)) {
+    cluster <- sample(0:(k-1), size = n, replace = TRUE)
+  } else {
+    ## recode cluster as 0,1,...,k-1
+    cluster <- factor(cluster)
+    if(length(levels(cluster)) != k)
+      stop("cluster vector does not have k clusters")
+    cluster <- as.integer(cluster) - 1
+    if(length(cluster) != n)
+      stop("data and length of cluster vector must match")
+  }
+  value <- kgroups_start(x, k, cluster, iter.max, distance = distance)
+  
   if (nstart > 1) {
     objective <- rep(0, nstart)
     objective[1] <- value$W
     values <- vector("list", nstart)
     values[[1]] <- value
     for (j in 2:nstart) {
-      values[[j]] <- .kgroupsStart(dst, k, iter.max, clus = NULL)
+      ## random initialization of cluster labels 
+      cluster <- sample(0:(k-1), size = n, replace = TRUE)
+      values[[j]] <- kgroups_start(x, k, cluster, iter.max, distance = distance)
+      
       objective[j] <- values[[j]]$W
     }
     best <- which.min(objective)
     value <- values[[best]]
   }
-
-obj  <- structure(list(
+  
+  obj  <- structure(list(
     call = match.call(),
-    cluster = value$cluster,
-    CL = value$CL,
-    sizes = as.vector(table(value$cluster)),
-    within = value$w,
-    W = sum(value$w),
+    cluster = value$cluster + 1,
+    sizes = value$sizes,
+    within = value$within,
+    W = sum(value$within),
     count = value$count,
-    iterations = value$iterations,
+    iterations = value$it,
     k = k),
     class = "kgroups")
-return (obj)
+  return (obj)
 }
-
-.kgroupsStart <- function(dst, k, iter.max, clus = NULL) {
-  ## k-groups clustering one complete iteration
-  ## or random re-start, which includes randomization of group labels
-  ## into approximately equal sized clusters
-  if (is.null(clus))
-    clus <- sample(1:k, size = nrow(dst), replace = TRUE)
-  value <- .onePass(dst, k, clus)
-  it <- 1
-  while (it < iter.max && value$count > 0) {
-    value <- .onePass(dst, k, clus = value$cluster,
-                      CL = value$CL, w = value$w)
-    it <- it + 1
-  }
-
-  return (list(
-    w = value$w,
-    W = sum(value$w),
-    CL = value$CL,
-    cluster = value$clus,
-    iterations = it,
-    count = value$count)
-  )
-}
-
-.onePass <- function(dst, k, clus, CL = NULL, w = NULL) {
-  ## k-groups clustering iteration over data set possibly moving
-  ## one point at a time to a new cluster to minimize within
-  ## cluster dispersions
-  n <- NROW(dst)
-  if (is.null(CL) || is.null(w)) {
-    ## then initialize both
-    w <- rep(0, k)
-    CL <- vector("list", k)
-    for (i in 1:k) {
-      CL[[i]] <- which(clus == i)
-      s <- sum(dst[CL[[i]], CL[[i]]])
-      w[i] <- s / (2 * length(CL[[i]]))
-    }
-  }
-
-  count <- 0
-  for (ix in 1:n) {
-    I <- clus[ix]
-    c1 <- CL[[I]]
-    n1 <- length(c1)
-    if (n1 < 2) next
-
-    Edst <- rep(0, k)
-    row_sum_ix <- rep(0, k)
-    for (J in 1:k) {
-      c2 <- CL[[J]]
-      n2 <- length(c2)
-      row_sum_ix[J] <- sum(dst[ix, c2])
-      Edst[J] <- (2 / n2) * (row_sum_ix[J] - w[J])
-    }
-    Jmin <- which.min(Edst)
-
-    if (Jmin != I) {
-      w[I] <- (n1 * w[I] - row_sum_ix[I]) / (n1 - 1)
-      n2 <- length(CL[[Jmin]])
-      w[Jmin] <- (n2 * w[Jmin] + row_sum_ix[Jmin]) / (n2 + 1)
-      clus[ix] <- Jmin
-      CL[[Jmin]] <- c(CL[[Jmin]], ix)
-      p <- which(CL[[I]] == ix)
-      CL[[I]] <- CL[[I]][-p]
-      count <- count + 1
-    }
-  }
-
-  return (list(
-    w = w,
-    CL = CL,
-    cluster = clus,
-    count = count)
-  )
-}
-
 
 
 print.kgroups <- function(x, ...) {
@@ -123,8 +59,9 @@ print.kgroups <- function(x, ...) {
 fitted.kgroups <- function(object, method = c("labels", "groups"), ...) {
   method = match.arg(method)
   if (method == "groups") {
-    CList <- vector("list", object$k)
-    for (i in 1:object$k) 
+    k <- object$k
+    CList <- vector("list", k)
+    for (i in 1:k) 
       CList[[i]] <- which(object$cluster == i)
     return (CList)
   }
